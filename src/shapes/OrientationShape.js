a9666caf20d9eb6d29ad0bedbec7f82a61654f0b
Arnau Martín <kyngo@kyngo.es>
2019-04-02 11:15:57 +0200

    Update OrientationShape.js

--- src/shapes/OrientationShape.js
+++ src/shapes/OrientationShape.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {
   HORIZONTAL_ORIENTATION,
   VERTICAL_ORIENTATION,
-  VERTICAL_SCROLLABLE
+  VERTICAL_SCROLLABLE,
 } from '../constants';
 
 export default PropTypes.oneOf([HORIZONTAL_ORIENTATION, VERTICAL_ORIENTATION, VERTICAL_SCROLLABLE]);
